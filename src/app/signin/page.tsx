41b038fc2488a45c93e3a8b553f042515e954219
Andrej Koller <andrejkoller@outlook.com>
2025-06-24 14:02:21 +0200

    Refactor authentication app: update dependencies, remove unused SVGs, enhance global styles, and implement new layout structure

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <section className="signin">
+      <div className="signin-container">
+        <div className="signin-content"></div>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <section className="signup">
+      <div className="signup-container">
+        <div className="signup-content"></div>
+      </div>
+    </section>
+  );
+}
